e21bfae89cf270a300dd0f1d4d96ea3d938fbdc5
Stewart Taylor <stewart.taylor1@gmail.com>
2014-12-16 16:14:08 -0800

    Cleaned up InputManager

--- DungeonCrawl/DungeonCrawl/DungeonCrawl/Engine/InputManager.cs
+++ DungeonCrawl/DungeonCrawl/DungeonCrawl/Engine/InputManager.cs
@@ -12,38 +12,18 @@ namespace DungeonCrawl.Engine
 {
     class InputManager
     {
-
-
-
         Director director;
         InputHelper input;
 
- 
-
         public  InputManager(InputHelper inputT ,Director directorT)
         {
-
             director = directorT;
             input = inputT;
-
         }
-
-
-
-
-
 
         public void update()
         {
-
-
-
-
-
-
-
             playerInput();
-
 
             if (MainData.developerMode == true)
             {
@@ -51,63 +31,38 @@ namespace DungeonCrawl.Engine
             }
 
             updateCamera();
-
         }
-
-
 
         private void playerInput()
         {
-
-
             if(MainData.xboxMode == false)
             {
-
                 pcInput();
-
             }
             else
             {
                 xboxInput();
             }
-
-
         }
-
-
-
-
 
         private void xboxInput()
         {
-
-     
-
-          
-
             if (input.IsCurPress(Buttons.Y))
             {
-                
                 director.player.useAction();
             }
 
             if (input.IsCurPress(Buttons.A))
             {
                 director.getPlayer().chargeAttack();
-
-
             }
             else if (input.IsOldPress(Buttons.A))
             {
                 director.getPlayer().attack();
             }
 
-
-
-
             if (input.IsCurPress(Buttons.LeftTrigger))
             {
-
                 director.player.defend();
 
                 if (input.IsNewPress(Buttons.A))
@@ -116,21 +71,15 @@ namespace DungeonCrawl.Engine
                 }
             }
 
-
-
             if (input.IsNewPress(Buttons.RightTrigger))
             {
                 director.getPlayer().getSpellManager().castFireBall(director.getPlayer());
-
             }
-
-
 
             float rotation = LeftThumbStickRotation();
 
             if (rotation != -9999)
             {
-
                 director.player.setRotation(LeftThumbStickRotation());
                 director.player.moveForward();
 
@@ -139,125 +88,93 @@ namespace DungeonCrawl.Engine
                     director.getPlayer().sprint();
                 }
             }
-
-
         }
 
 
+        public float LeftThumbStickRotation() 
+        {
+            float rotation = -9999;
+
+            Vector2 left = GamePad.GetState(PlayerIndex.One,GamePadDeadZone.Circular).ThumbSticks.Left;
+            if ((left.X != 0.0f) & (left.Y != 0.0f)) rotation = (float)(double)Math.Atan2((double)left.Y * -1, (double)left.X);
+
+            return rotation;
+        } 
 
 
-        
-	        
-	        public float LeftThumbStickRotation() 
-	        {
-                float rotation = -9999;
+        private void pcInput()
+        {
+            if (input.IsCurPress(Keys.W))
+            {
+                director.player.moveForward();
+            }
 
-                Vector2 left = GamePad.GetState(PlayerIndex.One,GamePadDeadZone.Circular).ThumbSticks.Left;
-                if ((left.X != 0.0f) & (left.Y != 0.0f)) rotation = (float)(double)Math.Atan2((double)left.Y * -1, (double)left.X);
-
-                return rotation;
-	        } 
-
-
-            private void pcInput()
+            if (input.IsCurPress(Keys.LeftAlt))
             {
                 if (input.IsCurPress(Keys.W))
                 {
-                    director.player.moveForward();
+                    director.getPlayer().sprint();
                 }
-
-                if (input.IsCurPress(Keys.LeftAlt))
-                {
-                    if (input.IsCurPress(Keys.W))
-                    {
-                        director.getPlayer().sprint();
-                    }
-                }
-
-                if (input.IsCurPress(Keys.S))
-                {
-                    director.player.moveBackward();
-                }
-
-                if (input.IsCurPress(Keys.Space))
-                {
-                    director.player.useAction();
-                }
-
-                if (input.IsCurPress(MouseButtons.LeftButton))
-                {
-                    director.getPlayer().chargeAttack();
-
-
-                }
-                else if (input.IsOldPress(MouseButtons.LeftButton))
-                {
-                    director.getPlayer().attack();
-                }
-
-
-
-
-                if (input.IsCurPress(MouseButtons.RightButton))
-                {
-
-                    director.player.defend();
-
-                    if (input.IsNewPress(MouseButtons.LeftButton))
-                    {
-                        director.player.defendPush();
-                    }
-                }
-
-
-
-                if (input.IsNewPress(Keys.D))
-                {
-                    director.getPlayer().getSpellManager().castFireBall(director.getPlayer());
-
-                }
-
-
-
-                director.player.pointTowardsPoint(new Vector2(input.MousePosition.X + director.camera.Pos.X - (500), input.MousePosition.Y + +director.camera.Pos.Y - (350)));
-
             }
 
+            if (input.IsCurPress(Keys.S))
+            {
+                director.player.moveBackward();
+            }
 
+            if (input.IsCurPress(Keys.Space))
+            {
+                director.player.useAction();
+            }
+
+            if (input.IsCurPress(MouseButtons.LeftButton))
+            {
+                director.getPlayer().chargeAttack();
+            }
+            else if (input.IsOldPress(MouseButtons.LeftButton))
+            {
+                director.getPlayer().attack();
+            }
+
+            if (input.IsCurPress(MouseButtons.RightButton))
+            {
+                director.player.defend();
+
+                if (input.IsNewPress(MouseButtons.LeftButton))
+                {
+                    director.player.defendPush();
+                }
+            }
+
+            if (input.IsNewPress(Keys.D))
+            {
+                director.getPlayer().getSpellManager().castFireBall(director.getPlayer());
+            }
+
+            director.player.pointTowardsPoint(new Vector2(input.MousePosition.X + director.camera.Pos.X - (500), input.MousePosition.Y + +director.camera.Pos.Y - (350)));
+        }
 
         private void testInput()
         {
-
             if (input.IsNewPress(Keys.R))
             {
                 director.generateLevel();
             }
-
 
             if (input.IsNewPress(Keys.A))
             {
                 director.getPlayer().teleport(new Vector2(input.MousePosition.X + director.camera.Pos.X - (500), input.MousePosition.Y + +director.camera.Pos.Y - (350)));
             }
 
-
             if (input.IsNewPress(Keys.T))
             {
                 director.test();
             }
 
-
             if (input.IsCurPress(Keys.Z))
             {
-                ///   director.getEffectManager().addFlame(new Vector2(input.MousePosition.X + director.camera.Pos.X - (500), input.MousePosition.Y + +director.camera.Pos.Y - (350)), 10);
-
-
-               //    director.getEffectManager().addFlame(director.getPlayer().getPosition(), 10, director.getPlayer().getRotation(), director.getPlayer().getSpeed());
-
                 director.getEffectManager().addTorchFlame(new Vector2(input.MousePosition.X + director.camera.Pos.X - (500), input.MousePosition.Y + +director.camera.Pos.Y - (350)), 1 );
-              //  director.getEffectManager().addFlame(director.getPlayer().getPosition(), 10, director.getPlayer().getRotation(), director.getPlayer().getSpeed());
             }
-
-
 
             if (input.IsNewPress(Keys.H))
             {
@@ -266,24 +183,15 @@ namespace DungeonCrawl.Engine
                 director.addCharacter(skel);
             }
 
-
             if (input.IsNewPress(Keys.C))
             {
                 Chest lo = new Chest(director, new Vector2(input.MousePosition.X + director.camera.Pos.X - (500), input.MousePosition.Y + +director.camera.Pos.Y - (350)));
                 director.addLevelObject(lo);
             }
-
-
         }
-
-
-
 
         private void updateCamera()
         {
-
-
-
             if (MainData.developerMode)
             {
 
@@ -328,18 +236,15 @@ namespace DungeonCrawl.Engine
                     CameraData.zoom = 0.3f;
                 }
 
-
                 if (input.IsNewPress(Keys.B))
                 {
                     director.dynamicCamera = !director.dynamicCamera;
                 }
-
             }
 
 
             if (director.dynamicCamera == false)
             {
-               // director.getCamera().Pos = new Vector2(director.getPlayer().getPosition().X, director.getPlayer().getPosition().Y);
                 director.getCamera()._pos.X =  MathHelper.SmoothStep(director.getCamera().Pos.X, director.getPlayer().getPosition().X, 0.2f);
                 director.getCamera()._pos.Y = MathHelper.SmoothStep(director.getCamera().Pos.Y, director.getPlayer().getPosition().Y, 0.2f);
                 
@@ -348,26 +253,13 @@ namespace DungeonCrawl.Engine
 
                 CameraData.xPosition = director.getCamera()._pos.X;
                 CameraData.yPosition = director.getCamera()._pos.Y;
-
             }
             else
             {
                 director.getCamera().Pos = new Vector2(CameraData.xPosition, CameraData.yPosition);
                 director.getCamera().Zoom = CameraData.zoom;
                 director.getCamera().Rotation = CameraData.rotation;
-
-
             }
-
-
-
-
         }
-
-
-
-
-
-
     }
 }
